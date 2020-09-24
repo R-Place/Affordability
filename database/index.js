484b31b1bbf2f88db0918d29dd0a2c198b8eb817
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-24 10:03:56 -0700

    Refactor for DRY code

--- database/index.js
+++ database/index.js
@@ -1,13 +1,29 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
-const data = require('../server/scripts/dummy_data.js')
 const connection = mysql.createConnection(mysqlConfig);
 
 connection.connect();
 
-
+var seedData = function(data) {
+  connection.query(`TRUNCATE homes`, (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      data.prices.forEach((price) => {
+        console.log(price, 'PRICE')
+        connection.query(`INSERT into homes (price) VALUES("${price}")`, (error, result) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log(result);
+          }
+        })
+      })
+    }
+  })
+};
 
 
 module.exports = {
-
+  seedData
 }
--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -1,29 +1,5 @@
-const mysql = require('mysql');
-const mysqlConfig = require('../../database/config.js');
-const data = require('./dummy_data.js')
-const connection = mysql.createConnection(mysqlConfig);
+const seedData = require('../../database/index.js')
+const data = require('./dummy_data')
 
-connection.connect();
-
-
-var seedData = function(data) {
-
-  connection.query(`TRUNCATE homes`, (error, result) => {
-    if (error) {
-      console.log(error);
-    } else {
-      data.prices.forEach((price) => {
-        console.log(price, 'PRICE')
-        connection.query(`INSERT into homes (price) VALUES("${price}")`, (error, result) => {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(result);
-          }
-        })
-      })
-    }
-  })
-};
-
-seedData(data);
+console.log(seedData);
+seedData.seedData(data);
