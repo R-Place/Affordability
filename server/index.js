12f90041f193cccdf042e803606ad4e99b17a577
JessicaWildermuth <jwildermuth536@gmail.com>
2020-09-30 12:49:25 -0700

    Remove extra express.static dir

--- server/index.js
+++ server/index.js
@@ -4,14 +4,13 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const port = 3000;
-const databaseFunctions = require('../database/index.js');
+const { Homes } = require('../database/index.js');
 
-app.use(express.static('client'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/affordability', (req, res) => {
-  databaseFunctions.getHomePrices((error, results) => {
+  Homes.getPrices((error, results) => {
     if (error) {
       res.send(error);
     } else {
